f0c355433c0635bb4539987b1382b5f477f023f0
Marco Guastalli <marco.guastalli@netcentric.biz>
2017-11-07 16:10:51 +0100

    migration html ok

--- m27-aem62-design-package/src/main/jcr_root/etc/designs/m27/website/clientlibs/m27.author/js/author.js
+++ m27-aem62-design-package/src/main/jcr_root/etc/designs/m27/website/clientlibs/m27.author/js/author.js
@@ -1,1 +1,19 @@
 /* Please include here all author script code needed */
+$(document).ready(function(){
+
+    $(".migrationButton").on('click', function() {
+        var buttonID = $(this).attr('id');
+        var number = buttonID.split("_")[1];
+        $.ajax({
+            type: "GET",
+            url: "/bin/m27/migration",
+            data: { migrationToDo: $(this).attr('val'),
+                paths: $('#migrationPaths_' + number).val(),
+                dryRun: $('#dryRun_' + number).is(':checked') },
+            success: function(responseData) {
+                $("#migrationResult").html(responseData);
+            }
+        });
+    });
+
+});
