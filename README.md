e8c1942c0d448390df13dad1d8afadf87a374c76
Marco Guastalli <marco.guastalli@netcentric.biz>
2017-11-23 17:27:37 +0100

    http://localhost:6201/content/m27/jora.jora.html

--- README.md
+++ README.md
@@ -1,54 +1,56 @@
 # MARCO27 AEM 6.2
 
-= AEM 6.2
+## AEM 6.2
 cd ~/dev/myStash/aem62-servers
 mvn cq-start
 
-= DEPLOY into AEM SERVERS
+## DEPLOY into AEM SERVERS
 mvn clean install -Pserver-package-deploy -Dcq.server=http://localhost:6200
 mvn clean install -Pserver-package-deploy -Dcq.server=http://localhost:6201
 
-= JSPINFO
+## JSPINFO
 http://localhost:6200/editor.html/content/m27/jspinfo.html
 http://localhost:6200/content/m27/jspinfo.html
 http://localhost:6201/content/m27/jspinfo.html
 
-= HTLINFO
+## HTLINFO
 http://localhost:6200/editor.html/content/m27/htlinfo.html
 http://localhost:6200/content/m27/htlinfo.html
 http://localhost:6201/content/m27/htlinfo.html
 
-= HTLTEMPLATE
+## HTLTEMPLATE
 http://localhost:6200/editor.html/content/m27/htltemplate.html
 http://localhost:6200/content/m27/htltemplate.html
 http://localhost:6201/content/m27/htltemplate.html
 
-= JORA
+## JORA
 http://localhost:6200/editor.html/content/m27/jora.html
 http://localhost:6200/content/m27/jora.html
 http://localhost:6201/content/m27/jora.html
+## JORA SERVLET
+http://localhost:6201/content/m27/jora.jora.html
 
-= TEXTANDIMAGE
+## TEXTANDIMAGE
 http://localhost:6200/editor.html/content/m27/textandimage.html
 http://localhost:6200/content/m27/textandimage.html
 http://localhost:6201/content/m27/textandimage.html
 
-= TEXTIMAGE
+## TEXTIMAGE
 http://localhost:6200/editor.html/content/m27/textimage.html
 http://localhost:6200/content/m27/textimage.html
 http://localhost:6201/content/m27/textimage.html
 
-= MIGRATION TOOL
+## MIGRATION TOOL
 http://localhost:6200/editor.html/etc/migrationtool.html
 http://localhost:6200/etc/migrationtool.html
 http://localhost:6201/etc/migrationtool.html
 
-= BASE ClientLib
+## BASE ClientLib
 http://localhost:6200/editor.html/content/m27/baseclientlib.html
 http://localhost:6200/content/m27/baseclientlib.html
 http://localhost:6201/content/m27/baseclientlib.html
 
-= BASE OSGi
+## BASE OSGi
 http://localhost:6200/editor.html/content/m27/baseosgi.html
 http://localhost:6200/content/m27/baseosgi.html
 http://localhost:6201/content/m27/baseosgi.html
--- m27-aem62-components/m27-aem62-components-core/src/main/java/net/marco27/aem6/components/osgi/jora/JoraServlet.java
+++ m27-aem62-components/m27-aem62-components-core/src/main/java/net/marco27/aem6/components/osgi/jora/JoraServlet.java
@@ -4,36 +4,35 @@ import java.io.IOException;
 import java.util.Dictionary;
 
 import javax.annotation.Nonnull;
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 
 import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
-import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/*
-@Service
-@Component(immediate = true, metatype = true)
+@Service(Servlet.class)
+@Component(metatype = false)
 @Properties(value = { @Property(name = "sling.servlet.resourceTypes", value = "sling/servlet/default"),
         @Property(name = "sling.servlet.selectors", value = { JoraServlet.SELECTOR_JORA }),
         @Property(name = "sling.servlet.extensions", value = { "html" }),
-        @Property(name = "sling.servlet.methods", value = { "GET" }) })
-*/
-@SlingServlet(resourceTypes = "sling/servlet/default", selectors = { JoraServlet.SELECTOR_JORA },
-        extensions = "html", methods = HttpConstants.METHOD_GET)
+        @Property(name = "sling.servlet.methods", value = { "GET", "POST" }) })
 public class JoraServlet extends SlingSafeMethodsServlet {
 
     private static final Logger LOG = LoggerFactory.getLogger(JoraServlet.class);
     public static final String SELECTOR_JORA = "jora";
 
     @Reference
-    private Jora jora;
+    private JoraService joraService;
 
     @Activate
     public void activate(final ComponentContext componentContext) {
@@ -45,8 +44,8 @@ public class JoraServlet extends SlingSafeMethodsServlet {
     @Override
     protected void doGet(@Nonnull SlingHttpServletRequest request, @Nonnull SlingHttpServletResponse response) throws ServletException,
             IOException {
-        this.handleMethodNotImplemented(request, response);
-        LOG.debug("Jora: " + jora.getJora());
-        response.getWriter().print(jora.getJora());
+        LOG.debug("Jora: " + joraService.getJora());
+        response.getWriter().print(joraService.getJora());
     }
+
 }
